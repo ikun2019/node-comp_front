008dc345df412957c53253af25cdb37558d81e99
ikun <0215.miyatto@gmail.com>
2023-04-11 05:35:39 +0900

    リセットメール送信機能

--- pages/auth/reset.vue
+++ pages/auth/reset.vue
@@ -3,15 +3,31 @@
     <div class="login-form">
       <div class="form-control">
         <label for="email">Email</label>
-        <input type="text" name="email" id="email">
+        <input type="text" name="email" id="email" v-model="email">
       </div>
-      <button type="submit" class="btn">Reset</button>
+      <button type="submit" class="btn" @click="onResetMail">Reset</button>
     </div>
   </main>
 </template>
 
 <script>
 export default {
-  
+  data() {
+      return {
+        email: ''
+      }
+  },
+  methods: {
+    async onResetMail() {
+      try {
+        let data = {
+          email: this.email
+        }
+        const response = await this.$axios.$post('/api/auth/reset', data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
 }
 </script>
