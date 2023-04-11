cb159303b8d4bc0c03951ed119995129edce4967
ikun <0215.miyatto@gmail.com>
2023-04-12 04:49:26 +0900

    reset画面のUI作成

--- pages/auth/new-password.vue
+++ pages/auth/new-password.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>New Password</h1>
+  <div class="login-form">
+    <div class="form-control">
+      <label for="password">Password</label>
+      <input type="text" name="password" id="password">
+    </div>
+    <button type="submit" class="btn">Update Password</button>
+  </div>
 </template>
 
 <script>
