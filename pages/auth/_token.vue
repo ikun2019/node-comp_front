76aa136bea7cad2b189ee0a168e7cd907a737b00
ikun <0215.miyatto@gmail.com>
2023-04-12 06:07:06 +0900

    パスワードリセット機能の実装

--- pages/auth/_token.vue
+++ pages/auth/_token.vue
@@ -2,11 +2,11 @@
   <div class="login-form">
     <div class="form-control">
       <label for="password">Password</label>
-      <input type="text" name="password" id="password">
+      <input type="text" name="password" id="password" v-model="password">
     </div>
     <input type="hidden" name="resetToken" :value="resetToken"><br>
     <input type="hidden" name="userId" :value="userId">
-    <button type="submit" class="btn">Update Password</button>
+    <button type="submit" class="btn" @click.prevent="onUpdatePassword">Update Password</button>
   </div>
 </template>
 
@@ -14,6 +14,7 @@
 export default {
   data() {
     return {
+      password: '',
       resetToken: '',
       userId: ''
     }
@@ -27,6 +28,21 @@ export default {
       }
     } catch (err) {
       console.log(err);
+    }
+  },
+  methods: {
+    async onUpdatePassword() {
+      try {
+        const data = {
+          password: this.password,
+          resetToken: this.resetToken,
+          userId: this.userId
+        }
+        const response = await this.$axios.$post('/api/auth/new-password', data);
+        this.$router.push('/');
+      } catch (err) {
+        console.log(err);
+      }
     }
   }
 }
