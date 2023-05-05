e670d09f66c7e363fc4a43a604a1f499d8047857
ikun <0215.miyatto@gmail.com>
2023-05-05 17:53:50 +0900

    paginationの実装

--- components/Pagenation.vue
+++ components/Pagenation.vue
@@ -1,44 +1,67 @@
 <template>
   <section class="pagination">
-    <a v-if="pageData.currentPage !== 1 && pageData.previosPage !== 1" href="/?page=1">1</a>
-    <template v-for="pageNumber in pageNumbers">
-      <a v-if="pageNumber !== pageData.currentPage && pageNumber !== pageData.previosPage && pageNumber !== pageData.nextPage" :key="pageNumber" :href="'/?page=' + pageNumber">{{ pageNumber }}</a>
-      <a v-else :key="pageNumber" :href="'/?page=' + pageNumber" class="active">{{ pageNumber }}</a>
-    </template>
-    <a v-if="pageData.hasNextPage" :href="'/?page=' + pageData.nextPage">{{ pageData.nextPage }}</a>
+    <ul class="d-flex">
+      <li v-for="(page, index) in pages" :key="index" class="d-block">
+        <a @click.prevent="changePage(page)" href="#">{{ page }}</a>
+      </li>
+    </ul>
   </section>
 </template>
 
 <style scoped>
-  .pagination {
-    margin-top: 2rem;
-    text-align: center;
-  }
-  .pagination a {
-    text-decoration: none;
-    color: #00695c;
-    padding: 0.5rem;
-    border: 1px solid #00695c;
-    margin: 0 1rem;
-  }
-  .pagination a:hover,
-  .pagination a:active,
-  .pagination a.active {
-    background: #00695c;
-    color: white;
-  }
+.pagination {
+  margin-top: 2rem;
+  text-align: center;
+}
+
+.d-flex {
+  display: flex;
+}
+
+.d-block {
+  display: block;
+}
+
+.pagination a {
+  text-decoration: none;
+  color: #00695c;
+  padding: 0.5rem;
+  border: 1px solid #00695c;
+  margin: 0 1rem;
+}
+
+.pagination a:hover,
+.pagination a:active,
+.pagination a.active {
+  background: #00695c;
+  color: white;
+}
 </style>
 
 <script>
 export default {
-  props: ['pageData'],
+  props: {
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+    currentPage: {
+      type: Number,
+      default: 1,
+    }
+  },
   computed: {
-    pageNumbers() {
-      let numbers = []
-      for (let i = 1; i <= this.pageData.pageCount; i++) {
-        numbers.push(i)
+    pages() {
+      const pages = [];
+      for (let i = 1; i <= this.totalPages; i++) {
+        pages.push(i);
       }
-      return numbers
+      return pages;
+    },
+  },
+  methods: {
+    changePage(page) {
+      this.$emit('page-changed', page);
     }
   }
 }
--- pages/feed/posts.vue
+++ pages/feed/posts.vue
@@ -4,36 +4,70 @@
     <div v-for="(post, index) in posts" :key="index">
       {{ post.title }}
     </div>
+    <pagination :current-page="currentPage" :total-pages="totalPages" @page-changed="onPageChanged" />
   </div>
 </template>
 
 <script>
+import Pagination from '@/components/Pagenation.vue';
 export default {
+  components: {
+    Pagination,
+  },
   data() {
     return {
-      posts: []
+      posts: [],
+      currentPage: 1,
+      totalPosts: 0,
     }
   },
-  async asyncData({ $axios }) {
+  async asyncData({ $axios, query }) {
     try {
+      const page = parseInt(query.page) || 1;
       const response = await $axios.$post('/graphql', {
         query: `
           query {
-            getPosts {
+            getPosts(page: ${page}) {
               posts {
                 id
                 title
                 content
               }
+              totalPosts
+              totalPages
             }
           }
         `
       });
       return {
         posts: response.data.getPosts.posts,
+        totalPosts: response.data.getPosts.totalPosts,
+        currentPage: page,
+        totalPages: response.data.getPosts.totalPages,
       }
     } catch (err) {
       console.error(err);
+    }
+  },
+  methods: {
+    async onPageChanged(page) {
+      this.currentPage = page;
+      const response = await this.$axios.$post('/graphql', {
+        query: `
+          query {
+              getPosts(page: ${page}) {
+                posts {
+                  id
+                  title
+                  content
+                }
+                totalPosts
+              }
+            }
+        `
+      });
+      this.posts = response.data.getPosts.posts;
+      this.$router.push(`/feed/posts?page=${page}`);
     }
   }
 }
