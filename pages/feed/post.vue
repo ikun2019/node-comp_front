0c5b5f80c959f48de16c012cfb85d33bb630bbb5
ikun <0215.miyatto@gmail.com>
2023-05-08 20:04:13 +0900

    画像アップロード機能の実装

--- pages/feed/post.vue
+++ pages/feed/post.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
+  <form>
     <div>
-      <label for="title">Title</label>
+      <label for="title">Title</label><br>
       <input type="text" name="title" id="title" v-model="title">
     </div>
     <div>
-      <label for="content">Content</label>
+      <label for="content">Content</label><br>
       <textarea name="content" id="content" cols="30" rows="10" v-model="content"></textarea>
     </div>
     <div>
-      <label for="imageUrl">Image URL</label>
-      <input type="text" name="imageUrl" id="imageUrl" v-model="imageUrl">
+      <label for="image">Image URL</label><br>
+      <input type="file" name="image" id="image" @change="onFileChange">
     </div>
-    <button type="submit" @click="onSubmit">Create</button>
-  </div>
+    <button type="submit" @click.prevent="onSubmit">Create</button>
+  </form>
 </template>
 
 <script>
@@ -22,38 +22,55 @@ export default {
     return {
       title: '',
       content: '',
-      imageUrl: '',
+      image: null,
     }
   },
   methods: {
     async onSubmit() {
+      const formData = new FormData();
+      formData.append('image', this.image);
+      console.log('form data =>', formData);
       try {
         const headers = {
-          Authorization: this.$auth.getToken('local')
+          Authorization: this.$auth.getToken('local'),
+          'Content-Type': 'multipart/form-data',
         }
-        console.log(headers);
-        const response = await this.$axios.$post('/graphql', {
-          query: `
-            mutation {
-              createPost(postInput: { title: "${this.title}", content: "${this.content}", imageUrl: "${this.imageUrl}" }) {
-                id
-                title
-                content
-                imageUrl
-                creator {
-                  name
-                }
-                createdAt
-              }
+        // 画像のアップロード
+        const response = await this.$axios.post('/upload', formData, { headers });
+        const imageUrl = response.data.url;
+
+        const mutation = `
+          mutation($input: PostInputData!) {
+            createPost(postInput: $input) {
+              title
+              content
+              imageUrl
             }
-          `
-        }, { headers });
-        window.console.log('response.data =>', response.data);
+          }
+        `
+        const variables = {
+          input: {
+            title: this.title,
+            content: this.content,
+            imageUrl: imageUrl,
+          }
+        }
+        const graphqlResponse = await this.$axios.$post('/graphql', {
+          query: mutation,
+          variables: variables,
+        });
+
+        console.log('GraphQL Response =>', graphqlResponse);
+
         this.$router.push('/');
       } catch (err) {
         console.error(err);
       }
-    }
+    },
+    onFileChange(e) {
+      this.image = e.target.files[0];
+      console.log('this image =>', this.image);
+    },
   },
 }
 </script>
