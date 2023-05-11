f2c4d4fd7eebd40fafbf995b7b5c12f98d691578
ikun <0215.miyatto@gmail.com>
2023-05-12 05:17:32 +0900

    削除機能の実装

--- pages/feed/_id.vue
+++ pages/feed/_id.vue
@@ -6,6 +6,7 @@
       <img :src="imageUrl" alt="">
     </div>
     <nuxt-link :to="{ path: '/feed/update', query: { id: postId } }">Update</nuxt-link>
+    <a href="#" @click.prevent="onDelete">Delete</a>
   </div>
 </template>
 
@@ -45,6 +46,32 @@ export default {
       imageUrl: response.data.getPost.imageUrl,
     }
   },
+  methods: {
+    async onDelete() {
+      try {
+        const headers = {
+          Authorization: this.$auth.getToken('local'),
+          'Content-Type': 'application/json',
+        }
+        const mutation = `
+          mutation($id: ID!) {
+            deletePost(id: $id)
+          }
+        `;
+        const variables = {
+          id: this.$route.params.id,
+        };
+        const response = await this.$axios.$post('/graphql', {
+          query: mutation,
+          variables: variables,
+        }, { headers });
+        console.log(response);
+        this.$router.push('/feed/posts');
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 }
 
 </script>
